da79a2119d115da8e9c8b7442f35f0c07d4fc78d
chenluyuan <chenluyuanit@gmail.com>
2016-12-21 22:44:41 +0800

    项目初始化

--- src/main/java/com/jaksona/security/service/auth/JakUserDetailsService.java
+++ src/main/java/com/jaksona/security/service/auth/JakUserDetailsService.java
@@ -1,12 +1,9 @@
 package com.jaksona.security.service.auth;
 
-import org.springframework.security.acls.model.AclService;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
-import javax.annotation.Resource;
 
 /**
  * @author jak
